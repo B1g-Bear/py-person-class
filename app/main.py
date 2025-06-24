c5b5d26d913dba2de9c53b1a7c83aab150a14e7c
Andrii Novikov <forytube2023@gmail.com>
2025-06-24 05:43:45 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,20 @@
 class Person:
-    people = {}
+    people: dict[str, "Person"] = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    persons = [Person(p["name"], p["age"]) for p in people]
+def create_person_list(people_data: list[dict]) -> list[Person]:
+    persons = [Person(person_data["name"], person_data["age"]) for person_data in people_data]
 
-    for p in people:
-        person = Person.people[p["name"]]
-        if "wife" in p and p["wife"] is not None:
-            person.wife = Person.people[p["wife"]]
-        elif "husband" in p and p["husband"] is not None:
-            person.husband = Person.people[p["husband"]]
+    for person_data in people_data:
+        person = Person.people[person_data["name"]]
+        if "wife" in person_data and person_data["wife"] is not None:
+            person.wife = Person.people[person_data["wife"]]
+        elif "husband" in person_data and person_data["husband"] is not None:
+            person.husband = Person.people[person_data["husband"]]
 
     return persons
